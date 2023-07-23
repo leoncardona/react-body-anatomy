3a7f4a8eeb9256a8791ca0481b3495295e6f8e36
leoncardona <noelleoncardona@gmail.com>
2023-07-23 18:54:39 +0200

    Modify rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,6 +5,16 @@ import dts from "rollup-plugin-dts";
 
 import packageJson from "./package.json" assert { type: "json" };
 
+const getTypesPath = (jsFile) => {
+  const pathInfo = parse(jsFile);
+  return format({
+    ...pathInfo,
+    base: "",
+    dir: `${pathInfo.dir}/types`,
+    ext: ".d.ts",
+  });
+};
+
 export default [
   {
     input: "src/index.ts",
@@ -18,18 +28,18 @@ export default [
         file: packageJson.module,
         format: "esm",
         sourcemap: true,
+        preserveModulesRoot: "src",
       },
     ],
     plugins: [
       resolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
-      sourcemaps(),
     ],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: getTypesPath(pkg.module ?? pkg.main),
+    output: [{ file: pkg.types, format: "esm" }],
     plugins: [dts()],
   },
 ];
